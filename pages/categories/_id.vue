45fc13201e49679c5922cda198c1ff7b794f4354
ghost-raven1 <sonofagronom@gmail.com>
2020-11-16 23:40:28 +0700

    init

---
+++ pages/categories/_id.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+
+  <a class="uk-button uk-button-primary uk-margin" @click="$router.go(-1)"><span uk-icon="arrow-left"></span> назад</a>
+
+  <client-only>
+  <div uk-grid>
+
+      <div class="uk-width-1-3@m">
+        <div v-for="product in category.products" v-bind:key="product.id" class="uk-margin">
+            <div class="uk-card uk-card-default">
+                <div class="uk-card-media-top">
+                    <img :src="'http://localhost:1337' + product.image.url" alt="" />
+                </div>
+                <div class="uk-card-body">
+                    <h3 class="uk-card-title">{{ product.name }} <span class="uk-badge">{{ product.price }}P</span></h3>
+                    <p>{{ product.description }}</p>
+                </div>
+                <div class="uk-card-footer">
+
+                  <button class="uk-button uk-button-primary">Добавить в корзину</button>
+                </div>
+            </div>
+        </div>
+      </div>
+
+
+      <div class="uk-width-expand@m">
+      </div>
+
+  </div>
+
+  </client-only>
+</div>
+</template>
+
+<script>
+import categoryQuery from '~/apollo/queries/category/category.gql'
+
+export default {
+  data() {
+    return {
+      category: Object
+    }
+  },
+  apollo: {
+    category: {
+      prefetch: true,
+      query: categoryQuery,
+      variables () {
+        return { id: this.$route.params.id }
+      }
+    }
+  }
+}
+</script>
